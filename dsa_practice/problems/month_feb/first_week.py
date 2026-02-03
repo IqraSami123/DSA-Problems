372a61ab89abdec7b7ed29aca4dbe94220af92e1
Iqra Sami <iqrasami998@gmail.com>
2026-02-03 20:11:45 +0500

    second day

--- dsa_practice/problems/month_feb/first_week.py
+++ dsa_practice/problems/month_feb/first_week.py
@@ -35,4 +35,52 @@ print("Multiply of the values are:", a * b)
 print("Division of the values are:", a // b)
 
 
-#  __________________first day 2_2_2026_____________________
+#  __________________second day 3_2_2026_____________________
+# Remove duplicates from a list
+
+def remove_duplicate(list) -> list:
+    unique_list = sorted(set(list))
+    return unique_list
+
+
+# Count frequency of each element in a list
+
+def count_frequancy(list) -> dict:
+    freq = {}
+    for item in list:
+        if item in freq:
+            freq[item] += 1
+        else:
+            freq[item] = 1
+    return freq
+
+
+# Access nested dictionary values
+def get_value(d, *keys):
+    for k in keys:
+        d = d.get(k)
+        if d is None:
+            return None
+    return d
+
+
+data = {
+    "user": {
+        "profile": {
+            "name": "Ali"
+        }
+    }
+}
+
+
+if __name__ == "__main__":
+    result = remove_duplicate([3, 1, 2, 3, 2, 1])
+    print(result)
+    result = count_frequancy([3, 1, 2, 3, 2, 1])
+    print(result)
+    name = get_value(data, "user", "profile", "name")
+    print(name)
+
+
+#  __________________third day 4_2_2026_____________________
+
