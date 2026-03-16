4ea74b0b0b160648c5f2c6b49f05a0f4a1e902f0
IqraSami123 <iqrasmi998@gmail.com>
2026-03-16 12:12:29 -0700

    implemented the docker

--- dsa_practice/dsa_practice/urls.py
+++ dsa_practice/dsa_practice/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dsa.views import health_check
 
 urlpatterns = [
+    path('', health_check, name='health'),
     path('admin/', admin.site.urls),
 ]
